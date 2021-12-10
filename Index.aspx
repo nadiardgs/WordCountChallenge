f78f90c4fdcdaa0df37f4f8a998d34983ceef212
Nadia R <nadia.rodrigues@alicredito.com.br>
2021-12-10 12:47:47 -0300

    Add Index.html to root

---
+++ Index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WordCountChallenge._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Word Count Challenge</h1>
+        <p></p>
+        <p class="lead">Word Count Challenge é uma aplicação que permite contar o número de palavras digitadas na caixa de texto.</p>
+        <p></p>
+        <p>
+            <asp:TextBox ID="textBox" TextMode="MultiLine" Height="100" Width="800" runat="server" ToolTip="Type your text here" Rows="10"></asp:TextBox>
+        &nbsp;&nbsp;
+            <asp:Label ID="lblMsg" runat="server" Visible="False" Font-Size="Medium" Font-Bold="True"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="submitButton" Text="Enviar" CssClass="btn btn-primary btn-lg" runat="server" OnClick="SubmitButton_Click"/>
+        </p>
+    </div>
+</asp:Content>
---
+++ Index.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WordCountChallenge
+{
+    public partial class _Default : Page
+    {
+        protected readonly string BlankSpace = " ";
+        protected readonly string LineFeed = "\n";
+        protected readonly string Tab = "\t";
+        protected readonly string CarriageReturn = "\r";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            textBox.Focus();
+        }
+
+
+        protected void ManageResponseLabel(bool visible, string message)
+        {
+            lblMsg.Visible = visible;
+            lblMsg.Text = message;
+        }
+
+        protected List<string> RemovePositionalCharacters(string text)
+        {
+            StringBuilder sb = new StringBuilder(text);
+
+            sb.Replace(LineFeed, BlankSpace);
+            sb.Replace(Tab, BlankSpace);
+            sb.Replace(CarriageReturn, BlankSpace);
+
+            Char.TryParse(BlankSpace, out char charBlankspace);
+
+            return sb.ToString().Split(charBlankspace).ToList();
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            ManageResponseLabel(false, string.Empty);
+
+            if (string.IsNullOrEmpty(textBox.Text.Trim()))
+            {
+                ManageResponseLabel(true, "Type some input to count the words!");
+            }
+            else
+            {
+                var splitText = RemovePositionalCharacters(textBox.Text);
+
+                //remove white spaces
+                splitText = splitText.Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+                lblMsg.Font.Bold = true;
+                ManageResponseLabel(true, $"You typed {splitText.Count} words.");
+            }
+        }
+    }
+}
